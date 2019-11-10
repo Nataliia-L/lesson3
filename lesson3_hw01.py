cb0917cf85f7dd6f6786bf17a397b7b012821ccc
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-10 13:57:35 +0200

    Create lesson3_hw01.py

---
+++ lesson3_hw01.py
@@ -0,0 +1,9 @@
+import random
+num_1 = random.uniform(1,20)
+print ('First_num =' , num_1)
+num_2 = random.uniform(2,30)
+print ('Second_num =' , num_2)
+val = num_1*num_2
+print ('Result_of_multiplication =' ,num_1*num_2)
+print ('Rounding_2 =' ,round (val, 2))
+print ('Rounding_4 =' ,round (val, 4))
