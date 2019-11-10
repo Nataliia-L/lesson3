f85ec356f742ee0e0c8aa628a9d430865bec07ed
Nataliia-L <57313896+Nataliia-L@users.noreply.github.com>
2019-11-10 13:59:08 +0200

    Create lesson3_hw04.py

---
+++ lesson3_hw04.py
@@ -0,0 +1,2 @@
+string = ('VZoFPzGvXwtbR+guMLszPhfvZotHwDm-emryhEXAaBW+-AbMdmEK+ztGDWyIzSFvjaFrHLzhoRqgsQraUdnSBGykFWNLOxTVKM+ynHqsVrQGEiqdKULoDVZcsZ-BpTWkDVR+jSgrfQZhUAdWCxtwOmTBQo+qWOlYyvGxnNpZFlTSrmaesQOotBLnVw!gOUrhz+F-DctdKWvTGhMUopoSPyFAXdJnY+DGFZQbOVgzaqsGvWjtBrJjBWhAFgryaTtwginpEsWtAuOxbgI-ZVunVAIuklBqTlSGUnevwEMqayHKF+sebTCtWcnhzUelA+iM+ivKleqrjoLsmrtHZOGfnHOWXYJpDwU!')
+print ('Result - ', string[10::11])
